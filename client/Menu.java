2cc348a844bc4ca525d6fcadf0ff405471c43dd6
RandomVOTVplayer <160535883+RandomVOTVplayer@users.noreply.github.com>
2026-04-13 14:32:09 -0400

    renamed Main.java to Menu.java

---
+++ client/Menu.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Menu {
+
+    public static void main(String[] args) {
+        System.out.println("Test");
+        Scanner userInput = new Scanner(System.in);
+
+        System.out.println("Please answer the following questions:");
+        System.out.println("What is your username?:");
+        String userName = userInput.nextline();
+    }
+}
